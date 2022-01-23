692a8d84d116224c754f5e56fb7c3d6b93182740
Will-Andrade <willian.santos.andra@outlook.com>
2022-01-23 18:25:47 -0400

    creates the modal basic functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+const displayModalBtn = document.querySelector('[data-js="display-modal"]');
+const modalBg = document.querySelector('[data-js="modal-bg"]');
+const hideModalBtn = document.querySelector('[data-js="hide-modal"]');
+const modalClose = document.querySelector('[data-js="modal-close"]');
+
+const closeModal = () => modalBg.classList.remove('bg-active');
+const displayModal = () => modalBg.classList.add('bg-active');
+
+displayModalBtn.addEventListener('click', displayModal);
+hideModalBtn.addEventListener('click', closeModal);
+modalClose.addEventListener('click', closeModal);
